13163c7b1866772762e12596d879c502760c635d
69esco69 <mysocialmedia69@yahoo.com>
2016-11-06 10:02:59 +0000

    Agency landing page
    Responsive Agency Landing Page

---
+++ js/main.js
@@ -0,0 +1,54 @@
+$(function(){
+  navStuff();
+  testimonialStuff();
+  portfolioStuff();
+});
+
+function portfolioStuff() {
+  $('.main-unit').first().addClass('show-thumb');
+  $('.sub-unit').first().addClass('show-thumb');
+  $('.sub-unit').on('click', function(){
+       var $this = $(this),
+             $siblings = $this.parent().children(),
+             position = $siblings.index($this);
+              $('.main-unit').removeClass('show-thumb').eq(position).addClass('show-thumb');
+         $siblings.removeClass('show-thumb');
+      $this.addClass('show-thumb'); 
+  });
+}
+
+
+
+
+function testimonialStuff() {
+  $('.face-unit').first().addClass('active');
+  $('.face-unit').on('click', function(){
+    var $this = $(this),
+          $siblings = $this.parent().children(),
+          position = $siblings.index($this);
+           $('.fb-unit').removeClass('active').eq(position).addClass('active');
+      $siblings.removeClass('active');
+   $this.addClass('active');  
+  });
+}
+
+
+
+
+function navStuff() {
+  $('.menu').on('click', function(){
+    $('.mobile-nav').toggleClass('show');
+    $('.menu').css({'transform': 'scale(0)'});
+  });
+
+
+  $('.mobile-nav nav a').on('click', function(){
+    $('.mobile-nav').removeClass('show');
+    $('.menu').css({'transform': 'scale(1)'});
+  });
+
+}
+
+
+
+
